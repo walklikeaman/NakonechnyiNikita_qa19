178b1f8ad436eaa074f20b696668c4e914549ab7
Nikita Nakonechnyi <whofarted@mail.ru>
2019-04-04 21:18:30 +0300

    first commit

---
+++ wikipedia-selenium-test/src/test/java/com/qa19/tests/FirstTest.java
@@ -0,0 +1,23 @@
+package com.qa19.tests;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+public class FirstTest {
+    WebDriver webDriver;
+
+    @BeforeClass
+    public void setUp() {
+
+    }
+    @Test
+    public void openChromeTest(){
+
+    }
+    @AfterClass
+    public void tearDown() {
+
+    }
+}
